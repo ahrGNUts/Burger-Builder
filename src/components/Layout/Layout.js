d5809a76c511dff6250e9af7a23965ccf72701d8
Patrick <revmydev@gmail.com>
2019-06-03 15:36:16 -0700

    burger builder container and spacing for header

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import classes from './Layout.module.css';
 
 const layout = ( props ) => (
     <>
         <div>Toolbar, sidedrawer, backdrop</div>
-        <main>
+        <main className={classes.Content}>
             {props.children}
         </main>
     </>
---
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+
+class BurgerBuilder extends Component {
+    render() {
+        return (
+            <>
+                <div>
+                    Burger
+                </div>
+                <div>
+                    Build controls
+                </div>
+            </>
+        );
+    }
+}
+
+export default BurgerBuilder;
